e96887a82bc0128246bb03436ad40fb7d83429a8
lennon_3000 <lennon_3000@hotmail.com>
2019-04-05 11:47:40 -0400

    Correção do Test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DEVOPS ESSENTIONS FLEXFORM")
+        self.assertRegex(result.data.decode(), "DEVOPS FLEXFORM")
 
 
 if __name__ == "__main__":
